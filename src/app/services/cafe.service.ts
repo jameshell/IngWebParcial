528e3b08bd6034b290a8ac100a367eecd60eb35d
James <jaimea111@gmail.com>
2024-11-02 04:43:06 -0500

    Agregando servicio de cafe

--- src/app/services/cafe.service.ts
+++ src/app/services/cafe.service.ts
@@ -2,11 +2,18 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Cafe } from '../models/Cafe.model';
+import { environment } from '../environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CafeService {
 
-  constructor() { }
+  private apiUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  obtenerCafe(): Observable<Cafe[]> {
+    return this.http.get<Cafe[]>(this.apiUrl);
+  }
 }
